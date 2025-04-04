8de39f9098cbf13bd4812e80f1b2057a36ecaa41
JValbjorn <julievalbjorn@live.dk>
2025-04-04 14:14:15 +0200

    changes to game play, try again button

--- js/script.js
+++ js/script.js
@@ -1,111 +1,130 @@
 "use strict";
 
-const targetDate = new Date().getTime() + (1000 * 480);
+const targetDate = new Date().getTime() + 1000 * 480;
+
+let gameEnded = false;
 
 // Countdown function to update the timer
 function updateCountdown() {
-    const now = new Date().getTime();
-    const timeLeft = targetDate - now;
+  const now = new Date().getTime();
+  const timeLeft = targetDate - now;
 
-    // Calculate days, hours, minutes, and seconds
-    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+  // Calculate days, hours, minutes, and seconds
+  const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
-    // Display the result in the countdown div
-    document.getElementById("countdown").innerHTML = 
-        `0${minutes}:${seconds}`;
+  // Display the result in the countdown div
+  document.getElementById("countdown").innerHTML = `0${minutes}:${seconds}`;
 
-    // Check if the countdown has ended
-    if (timeLeft > 0) {
-        setTimeout(updateCountdown, 1000); // Call updateCountdown again after 1 second
-    } else {
-        document.getElementById("countdown").innerHTML = "The event has started!";
+  // Check if the countdown has ended
+  if (timeLeft > 0 && !gameEnded) {
+    setTimeout(updateCountdown, 1000); // Call updateCountdown again after 1 second
+  } else {
+    document.getElementById("countdown").innerHTML = "The event has ended!";
+    if (!gameEnded) {
+      gameLoss();
     }
+  }
+}
+
+let currentIndex = 0;
+let completedAssignments = 0;
+let failedAssignments = 0;
+
+function gameWin() {
+  document.getElementById("display").innerHTML = "You Beat Mr Evil";
+  document.getElementById("nextButtonSkip").hidden = true; // Disable the button when reaching the last item
+  document.getElementById("nextButtonPlus").hidden = true; // Disable the button when reaching the last item
+  document.getElementById("nextButtonMin").hidden = true; // Disable the button when reaching the last item
+  document.getElementById("backToHome").hidden = true; // Disable the button when reaching the last item
+  gameEnded = true;
+  document.getElementById("restartGameId").hidden = false; //This button should restart the game
+}
+
+function gameLoss() {
+  document.getElementById("display").innerHTML = "Hahahaha ha silly human!";
+  document.getElementById("nextButtonSkip").hidden = true; // Disable the button when reaching the last item
+  document.getElementById("nextButtonPlus").hidden = true; // Disable the button when reaching the last item
+  document.getElementById("nextButtonMin").hidden = true; // Disable the button when reaching the last item
+  document.getElementById("backToHome").hidden = true; // Disable the button when reaching the last item
+  gameEnded = true;
+  document.getElementById("restartGameId").hidden = false; //This button should restart the game
+}
+
+function checkGameState() {
+  if (failedAssignments > 4) {
+    gameLoss();
+  }
+  if (completedAssignments > 9) {
+    gameWin();
+  }
+
+  if (currentIndex > items.length - 1) {
+    gameLoss();
+  }
 }
 
 // Initial call to start the countdown
 updateCountdown();
 // Define an array with some items
 // Keep track of the current index and click count
-let currentIndex = 0;
-let clickCount = 0;
 
 // Function to show the next item and update the click counter
 function showNextItemPlus() {
-  if (currentIndex < items.length - 1) {
-    currentIndex++; // Increment the index to show the next item
-    document.getElementById('display').innerText = items[currentIndex];
-    
-    clickCount++; // Increment the click counter
-    document.getElementById('clickCount').innerText = clickCount;
-  } else {
-    if (clickCount < 5){
-      document.getElementById('display').innerHTML = "Hahahaha ha silly human!"
-      document.getElementById('nextButtonSkip').disabled = true; // Disable the button when reaching the last item
-    }
-    if (clickCount > 5){
-      document.getElementById('display').innerHTML = "You Beat Mr Evil"
-      document.getElementById('nextButtonSkip').disabled = true; // Disable the button when reaching the last item
-  }
-  }
+  currentIndex++; // Increment the index to show the next item
+  document.getElementById("display").innerText = items[currentIndex];
+
+  completedAssignments++; // Increment the click counter
+  document.getElementById("clickCount").innerText =
+    completedAssignments - failedAssignments;
+
+  checkGameState();
 }
 
 function showNextItemMin() {
-    if (currentIndex < items.length - 1) {
-      currentIndex++; // Increment the index to show the next item
-      document.getElementById('display').innerText = items[currentIndex];
-      
-      clickCount--; // Increment the click counter
-      document.getElementById('clickCount').innerText = clickCount;
-    } else {
-      if (clickCount < 5){
-        document.getElementById('display').innerHTML = "Hahahaha ha silly human!"
-        document.getElementById('nextButtonSkip').disabled = true; // Disable the button when reaching the last item
-      }
-      if (clickCount > 5){
-        document.getElementById('display').innerHTML = "You Beat Mr Evil"
-        document.getElementById('nextButtonSkip').disabled = true; // Disable the button when reaching the last item
-  }
-  }
+  currentIndex++; // Increment the index to show the next item
+  document.getElementById("display").innerText = items[currentIndex];
+
+  failedAssignments++; // Increment the click counter
+  document.getElementById("clickCount").innerText = completedAssignments - failedAssignments;
+  checkGameState();
 }
 
+// the skip counter
 let amountSkipped = 0;
 
+function ShowAmountOfSkipsLeft() {
+  document.getElementById("skipButtonCount").innerText = 3 - amountSkipped;
+}
+
 function showNextItemSkip() {
-    if (amountSkipped > 2) {
-      return;
-    }
-
-    amountSkipped++;
-
-    ShowAmountOfSkipsLeft();
-
-    if (currentIndex < items.length - 1) {
-      currentIndex++; // Increment the index to show the next item
-      document.getElementById('display').innerText = items[currentIndex];
-    } else {
-    if (clickCount < 5){
-      document.getElementById('display').innerHTML = "Hahahaha ha silly human!"
-      document.getElementById('nextButtonSkip').disabled = true; // Disable the button when reaching the last item
+  if (amountSkipped > 2) {
+    return;
   }
-    if (clickCount > 5){
-       document.getElementById('display').innerHTML = "You Beat Mr Evil"
-        document.getElementById('nextButtonSkip').disabled = true; // Disable the button when reaching the last item
-  }   
-  }
+
+  amountSkipped++;
+
+  ShowAmountOfSkipsLeft();
+
+  currentIndex++; // Increment the index to show the next item
+  document.getElementById("display").innerText = items[currentIndex];
+
+  checkGameState();
 }
 
 ShowAmountOfSkipsLeft();
 
-function ShowAmountOfSkipsLeft() {
-  document.getElementById('skipButtonCount').innerText = 3 - amountSkipped;
-}
-
 // Show the first item initially
-document.getElementById('display').innerText = items[currentIndex];
+document.getElementById("display").innerText = items[currentIndex];
 // Add event listener to the button to show the next item and update the click counter when clicked
-document.getElementById('nextButtonPlus').addEventListener('click', showNextItemPlus);
+document
+  .getElementById("nextButtonPlus")
+  .addEventListener("click", showNextItemPlus);
 // Add event listener to the button to show the next item and update the click counter when clicked
-document.getElementById('nextButtonMin').addEventListener('click', showNextItemMin);
+document
+  .getElementById("nextButtonMin")
+  .addEventListener("click", showNextItemMin);
 // Add event listener to the button to show the next item and update the click counter when clicked
-document.getElementById('nextButtonSkip').addEventListener('click', showNextItemSkip);
+document
+  .getElementById("nextButtonSkip")
+  .addEventListener("click", showNextItemSkip);
